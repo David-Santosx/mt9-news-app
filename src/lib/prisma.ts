9d52b407131ec714400743bcd4888675b84c8def
David-Santosx <willians.sant@proton.me>
2025-07-21 12:34:08 -0400

    feat: add @prisma/extension-accelerate for enhanced database performance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@/../node_modules/.prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
